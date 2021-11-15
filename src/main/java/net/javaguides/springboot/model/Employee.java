eebed4609e3de09c999719a4df7adca04a9139e8
MD Pear E Habib <habib.austcse31@gmail.com>
2021-11-15 12:26:26 +0600

    employee.java

--- src/main/java/net/javaguides/springboot/model/Employee.java
+++ src/main/java/net/javaguides/springboot/model/Employee.java
@@ -1,8 +1,14 @@
 package net.javaguides.springboot.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+@Table(name="employees")
 public class Employee {
 	
 	private long id;
@@ -20,29 +26,43 @@ public class Employee {
 		this.lastName = lastName;
 		this.email = email;
 	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	public long getId() {
 		return id;
 	}
 	public void setId(long id) {
 		this.id = id;
 	}
+	
+	@Column(name="first_name",nullable=false)
 	public String getFirstName() {
 		return firstName;
 	}
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+	
+	@Column(name="last_name",nullable=false)
 	public String getLastName() {
 		return lastName;
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+	
+	@Column(name="email",nullable=false)
 	public String getEmail() {
 		return email;
 	}
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
 	}
 	
 	
